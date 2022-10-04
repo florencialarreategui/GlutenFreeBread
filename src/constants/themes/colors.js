80a851a8f94074b7b9688bd8508f3e07843d8bde
florencialarreategui <florencialarreategui@gmail.com>
2022-10-03 21:56:33 -0300

    agrego colores como constantes

---
+++ src/constants/themes/colors.js
@@ -0,0 +1,9 @@
+export default{
+    primary:"#EDB183",
+    secondary:"#F1C5A8",
+    backgraound:"#F4D8CD",
+    accent: "#F15152",
+    secondaryAccent:"#1E555C",
+    typography:"#3A2E39",
+    
+}
